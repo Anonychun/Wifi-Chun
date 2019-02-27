af20225adb5d7cc472ce394af725ee52c8e9185c
Achmad Chun-Chun Winata Adi <41361100+Anonychun@users.noreply.github.com>
2019-02-27 22:48:06 +0700

    [Anonychun] First Commit

--- wifichun.cpp
+++ wifichun.cpp
@@ -17,71 +17,47 @@
 #include <iostream>
 #include <fstream>
 
-// https://github.com/Anonychun
-
 using namespace std;
 
-// prototipe, deklarasi fungsi
 void logo();
 int getOption();
 
-// main, program utama
 int main() {
 
-	// deklarasi variabel
 	int pilihan = getOption();
 	char is_continue;
 
-	// enum untuk membuat opsi pilihan angka menjadi kata
-	enum option{REPOSITORY = 1, INSTALL, DISCONNECT, FLOODING, RESTART, EXIT = 0};
+	enum option{DISCONNECT = 1, FLOODING, RESTART, EXIT = 0};
 
-	// control program
 	while (pilihan != EXIT) {
 
-		// control pilihan user
 		switch (pilihan) {
 
-			case REPOSITORY: {
-				// update dan upgrade repository
-				system("clear");
-				system("apt update && apt upgrade");
-				break;
-
-			} case INSTALL: {
-				// install mdk3 dan aircrack-ng
-				system("clear");
-				system("apt install mdk3 aircrack-ng");
-				break;
-
-			} case DISCONNECT: {
-				// mengeksekusi file python
+			case DISCONNECT: {
 				system("clear");
 				system("python3 disconnect.py");
 				break;
 
 			} case FLOODING: {
-				// mengeksekusi file python
 				system("clear");
 				system("python3 flooding.py");
 				break;
 
 			} case RESTART: {
-				// mengeksekusi file python
 				system("clear");
 				system("python3 restart.py");
 				break;
 
 			} default: {
-				cout << "Pilihan yang anda masukan salah\n" << endl;
+				cout << "You entered the wrong choice\n" << endl;
 				break;
 			}
 
 		}
 
-		// label konfirmasi untuk melanjutkan atau memberhentikan program
 		label_continue:
 
-		cout << "Lanjutkan? (y/n) : ";
+		cout << "Continue? (y/n) : ";
 		cin >> is_continue;
 
 		if ((is_continue == 'y') || (is_continue == 'Y')) {
@@ -93,35 +69,35 @@ int main() {
 		}
 	}
 
-	// akhir dari program
-	cout << "\nProgram Telah Berhenti, Terima Kasih ^_^" << endl;
+	cout << "\n[+] Quitting" << endl;
 
 	cin.get();
 	return 0;
 }
 
 void logo() {
-	cout << "  _   _   _   _   _   _   _   _   _  " << endl;
-	cout << " / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ " << endl;
-	cout << "( W | i | F | i | - | C | h | u | n )" << endl;
-	cout << " \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ " << endl;
-	cout << endl;
+	ifstream file;
+	string data;
+
+	file.open("logo.txt");
+	while (!file.eof()) {
+		getline(file, data);
+		cout << data << endl;
+	} cout << endl;
 }
 
 int getOption() {
 	int input;
 	system("clear");
 	logo();
-	cout << "Penetration Network !!!" << endl;
+	cout << "https://github.com/Anonychun" << endl;
 	cout << "==============================" << endl;
-	cout << "1. Update & Upgrade Repository" << endl;
-	cout << "2. Install MDK3 & Aircrack-ng" << endl;
-	cout << "3. Disconnect WiFi" << endl;
-	cout << "4. Flooding WiFi" << endl;
-	cout << "5. Restart Network" << endl;
+	cout << "1. Disconnect WiFi" << endl;
+	cout << "2. Flooding WiFi" << endl;
+	cout << "3. Restart Network" << endl;
 	cout << "0. Exit" << endl;
 	cout << "==============================" << endl;
-	cout << "Pilih [1-5]? : ";
+	cout << "Select [1-3]? : ";
 	cin >> input;
 	return input;
 }
